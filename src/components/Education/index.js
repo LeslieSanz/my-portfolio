6c1244bf207e0eaa51026ecb7c611a7de934f77f
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2024-12-29 14:47:49 -0500

    first commit

---
+++ src/components/Education/index.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import EducationCard from "../Cards/EducationCard";
+import { education } from "../../data/constants";
+
+// Estilos generales
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 1;
+  padding: 40px 0px 80px 0px;
+  @media (max-width: 960px) {
+    padding: 0px;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1350px;
+  width: 100%;
+  padding: 80px 0;
+  gap: 12px;
+`;
+
+const Title = styled.div`
+  font-size: 42px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+    font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+  font-size: 18px;
+  text-align: center;
+  max-width: 600px;
+  color: ${({ theme }) => theme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const TimelineSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1000px; /* Este es el ancho máximo de la línea de tiempo */
+  margin-top: 10px;
+`;
+
+const Education = () => {
+  return (
+    <Container id="education">
+      <Wrapper>
+        <Title>Education</Title>
+        <Desc>
+          My education has been a journey of self-discovery and growth. My
+          educational details are as follows.
+        </Desc>
+        <TimelineSection>
+          <Timeline position="alternate">
+            {education.map((item, index) => (
+              <TimelineItem key={index}>
+                <TimelineSeparator>
+                  <TimelineDot variant="outlined" color="secondary" />
+                  {index !== education.length  && (
+                    <TimelineConnector style={{ background: "#854CE6" }} />
+                  )}
+                </TimelineSeparator>
+                <TimelineContent sx={{ py: "12px", px: 2 }}>
+                  <EducationCard education={item} />
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </Timeline>
+        </TimelineSection>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Education;
---
+++ src/data/constants.js
@@ -0,0 +1,243 @@
+export const Bio = {
+    name: "Leslie Sanchez",
+    roles: [
+      "Software Student",
+      "Programmer",
+      "Tech Nerd",
+    ],
+    description:
+      "I am a dedicated Software Engineering student with a strong enthusiasm for problem-solving and innovation. My goal is to continuously expand my skills and contribute to impactful projects. With a focus on quality and a collaborative mindset.",
+    github: "https://github.com/LeslieSanz",
+    resume:
+      "https://drive.google.com/file/d/1mjOILZkkFC_o9838LCqrpyogvc4brGpo/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/leslie-sanchezman/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://cdn-icons-png.flaticon.com/512/226/226777.png",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "PHP",
+          image:
+            "https://assets.ohmynewst.com/newsletters/clzo0m1ww002vbl9ql0c2y9p3/logo/5d362c87a7f4baa701e2252cf605f6.png",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "SQL Server",
+          image: "https://img.icons8.com/color/512/microsoft-sql-server.png",
+        },
+      ],
+    },
+    {
+      title: "DevOps",
+      skills: [
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Google Cloud",
+          image:
+            "https://cdn.worldvectorlogo.com/logos/google-cloud-2.svg",
+        },
+        {
+          name: "Azure",
+          image:
+            "https://cdn.worldvectorlogo.com/logos/azure-1.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Excel",
+          image:
+            "https://images.seeklogo.com/logo-png/26/1/excel-logo-png_seeklogo-266583.png?v=1958568720680711152",
+        },
+        
+        {
+          name: "Power BI",
+          image:
+            "https://w7.pngwing.com/pngs/252/727/png-transparent-power-bi-business-intelligence-microsoft-analytics-microsoft-text-rectangle-logo-thumbnail.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/1667px-Figma-logo.svg.png",
+        },
+      ],
+    },
+  ];
+  
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "EduTec",
+      date: "Apr 2024 - May 2024",
+      description:
+        "EduTec is a comprehensive desktop application developed to personalize the academic tutoring process. It serves as a platform that connects first-cycle students with experienced mentors, providing tailored guidance to help students navigate their academic challenges and enhance their learning experience. The application offers a user-friendly interface for scheduling, managing, and tracking tutoring sessions, ensuring a seamless and efficient tutoring experience for both students and mentors.",
+      image:
+        "https://imgur.com/fFVXwxZ.png",
+      tags: [
+        "Java",
+        "JavaSwing",
+        "MySQL",
+        "NetBeans",
+        "GitHub",
+      ],
+      category: "desktop app",
+      github: "https://github.com/makkakoo/proyecto-EduTec-integrador",
+    },
+    {
+      id: 0,
+      title: "HappyPets",
+      date: "Sep 2023 - Dec 2023",
+      description:
+        "HappyPets is a web application designed to manage the adoption process of rescued dogs and cats, developed to streamline tracking and follow-up for an animal welfare organization. The platform allows users to report animals in need of rescue, make voluntary donations, and stay informed through an integrated blog that shares valuable updates and advice on pet care. This project aims to create a user-friendly environment for animal adoption while also encouraging community involvement and support for rescued animals.",
+      image:
+        "https://imgur.com/fOTPKAy.png",
+      tags: ["HTML", "CSS", "JavaScript", "PHP", "MySQL", "VSCode"],
+      category: "web app",
+      github: "https://github.com/LeslieSanz/HappyPets-ProyectoWeb",
+    },
+    {
+      id: 1,
+      title: "WikiCausa",
+      date: "Jun 2024 - Jul 2024",
+      description:
+        "WikiCausa is a desktop application designed to generate personalized weekly menus based on the user's specific dietary needs. It features a comprehensive database of recipes for users to follow and includes the ability to register nutritional data, making it a useful tool for those looking to manage their meals effectively and maintain a balanced diet.",
+      image:
+        "https://imgur.com/fAwNibP.png",
+      tags: [
+        "Java",
+        "Java Swing",
+        "MySQL",
+        "Netbeans",
+        "Figma",
+        "GitHub",
+      ],
+      category: "desktop app",
+      github: "https://github.com/LeslieSanz/proyecto-WikiCausa-integrador",
+    },
+    {
+      id: 2,
+      title: "TopTech",
+      date: "Jun 2023 - Jul 2023",
+      description:
+        "TopTech is an inventory management system designed to track the stock of technology products in a warehouse. The application allows users to monitor the movement of items, ensuring efficient control of incoming and outgoing stock.",
+      image:
+        "https://imgur.com/6nqsTpw.png",
+      tags: ["Java", "JavaSwing", "MySQL", "Netbeans", "GitHub",],
+      category: "desktop app",
+      github: "https://github.com/LeslieSanz/inventario-toptech-java",
+    },
+    {
+      id: 3,
+      title: "Architecture Courses Website",
+      date: "May 2024 - Jul 2024",
+      description:
+        "This e-commerce website offers access to architecture courses provided by a professional in the field. The platform allows users to browse and enroll in various courses related to architecture, fostering learning and skill development in the field.",
+      image:
+        "https://imgur.com/yZ1GIVh.png",
+      tags: ["Java", "Java Server Faces", "HTML", , "Bootstrap", "MySQL", "NetBeans"],
+      category: "web app",
+      github: "https://github.com/LeslieSanz/WebIntegrado-JSF",
+    },
+    {
+      id: 10,
+      title: "Crono-Libertador",
+      date: "Nov 2024 - Dic 2024",
+      description:
+        "A captivating video game developed with Godot. Players can immerse themselves in an interactive universe, face strategic challenges, and enjoy a seamless, engaging experience.",
+      image:
+        "https://imgur.com/1iAedJq.png",
+      tags: ["Godot", "GDScript", "PixelArt"],
+      category: "videogame",
+      github: "https://github.com/LeslieSanz/Godot_project_videogame",
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://imgur.com/2OFRxvh.png",
+      school: "Universidad Tecnológica del Perú, Lima Centro",
+      date: "Aug 2021 - Present",
+      grade: "On track",
+      desc: "I am currently pursuing a Bachelor's degree in Software Engineering at Universidad Tecnológica del Perú. I have completed several courses, including Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks. I have also worked on team projects applying these concepts.",
+      degree: "Bachelor of Engineering - Software Engineering",
+    },
+    {
+      id: 1,
+      img: "https://imgur.com/bU0ptlF.png",
+      school: "Asociación Cultural Peruano Británica",
+      date: "Aug 2023 - Present",
+      grade: "Proficiency Level Verified",
+      desc: "I am currently enrolled in an advanced English program at Asociación Cultural Peruano Británica. This program focuses on developing advanced skills in reading, writing, speaking, and listening, preparing me for academic and professional goals.",
+      degree: "Advanced English Program",
+    },
+    {
+      id: 2,
+      img: "https://imgur.com/64tDJcz.png",
+      school: "Colegio Saco Oliveros, Chorrillos",
+      date: "Mar 2018 - Dec 2020",
+      grade: "Sucessfully Completed",
+      desc: "I completed my secondary education at Colegio Saco Oliveros in Chorrillos. During this time, I achieved a strong academic record and built a solid foundation in science and mathematics, which prepared me for my university studies.",
+      degree: "Secondary Education",
+    },
+  ];
+  
+  
